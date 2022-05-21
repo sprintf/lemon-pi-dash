b7a64fccf6b01bddf25a4351951ecfde46bf319c
sprintf <paul@normingtons.org>
2022-05-20 19:30:10 -0700

    adds get mapping

--- src/main/kotlin/com/normtronix/dash/WebController.kt
+++ src/main/kotlin/com/normtronix/dash/WebController.kt
@@ -13,6 +13,7 @@ class WebController {
     @Value("\${mapsApiKey}")
     lateinit var mapsApiKey:String
 
+    @GetMapping("/live_track_map")
     fun trackMap(@RequestParam(name="track") track: String,
                  model: Model): String {
         model.addAttribute("trackCode", track)
