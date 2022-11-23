e43966444e478c7a56866a6b240d3e42acca31aa
sprintf <paul@normingtons.org>
2022-11-22 20:42:18 -0800

    fixes firestore dependency in github actions

--- src/test/kotlin/com/normtronix/dash/DashApplicationTests.kt
+++ src/test/kotlin/com/normtronix/dash/DashApplicationTests.kt
@@ -3,9 +3,11 @@ package com.normtronix.dash
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
-
-@SpringBootTest
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit4.SpringRunner
+@SpringBootTest(classes = [AuthService::class])
 @Import(TestFirestoreConfiguration::class)
+@TestPropertySource(locations=["classpath:application.properties"])
 class DashApplicationTests {
 
 	@Test
