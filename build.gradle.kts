5af0c5593f99cf265329daa32121687920eae9d9
PaulN <paul@normingtons.org>
2025-06-15 20:26:48 -0700

    switches to use library for protobuf / grpc

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val grpcVersion: String by project
 val protobufVersion: String by project
-val protobufPluginVersion: String by project
 
 plugins {
 	idea
@@ -19,6 +18,14 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
+	maven {
+		name = "GitHubPackages"
+		url = uri("https://maven.pkg.github.com/sprintf/lemon-pi-protos")
+		credentials {
+			username = System.getenv("GITHUB_ACTOR")
+			password = System.getenv("GITHUB_TOKEN")
+		}
+	}
 }
 
 extra["springCloudGcpVersion"] = "2.0.6"
@@ -26,7 +33,7 @@ extra["springCloudVersion"] = "2020.0.4"
 extra["gcpLibrariesVersion"] = "26.1.4"
 
 dependencies {
-	implementation(project(":lemon-pi-protos"))
+	implementation("com.normtronix:lemon-pi-protos:1.0")
 
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -34,21 +41,30 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.5.2")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.1")
 
 	implementation("io.grpc:grpc-protobuf:${grpcVersion}")
 	implementation("io.grpc:grpc-stub:${grpcVersion}")
+	implementation("com.google.protobuf:protobuf-java:${protobufVersion}")
+
 	implementation("io.grpc:grpc-kotlin-stub:0.1.5")
 	implementation("jakarta.validation:jakarta.validation-api")
 	implementation("org.hibernate.validator:hibernate-validator")
 	implementation("com.google.cloud:google-cloud-firestore")
+	implementation("com.google.code.gson:gson:2.10.1")
+	implementation("com.squareup.okhttp3:okhttp:4.11.0")
+	implementation("com.slack.api:slack-api-client:1.27.1")
 
 	runtimeOnly("io.grpc:grpc-okhttp:${grpcVersion}")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.mockk:mockk:1.12.2")
+
+	testImplementation("org.testcontainers:testcontainers:1.19.0")
+	testImplementation("org.testcontainers:junit-jupiter:1.19.0")
+	testImplementation("org.testcontainers:gcloud:1.19.0")
 }
 
 dependencyManagement {
